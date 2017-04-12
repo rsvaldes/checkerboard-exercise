4c6ba6e3fb35160f657804df520261aa33d88921
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2017-04-12 16:17:57 -0600

    alternating two colors checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// Your JS goes here
+var body = document.getElementsByTagName("body")[0];
+
+for (var i = 0; i < 81; i++) {
+  var tile = document.createElement("div");
+  tile.style.width = "11.1%";
+  tile.style.float = "left";
+  tile.style.paddingBottom = "11.1%";
+  if (i % 2 === 0) {
+    tile.style.backgroundColor = "pink";
+    body.appendChild(tile);
+  }
+  else {
+    tile.style.backgroundColor = "purple";
+    body.appendChild(tile);
+  }
+
+}
+
+
+
+// tile.style.backgroundColor = "purple";
+// tile2.style.width = "11.1%";
+// tile2.style.float = "left";
+// tile2.style.paddingBottom = "11.1%";
+// tile2.style.backgroundColor = "pink";
+
+// body.appendChild(tile);
