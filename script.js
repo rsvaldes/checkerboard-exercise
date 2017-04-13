30b836dd26ad58bcd5a4502c11e5467663f923dd
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2017-04-12 18:36:44 -0600

    adding random colors checkerboard

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
 var body = document.getElementsByTagName("body")[0];
 
-for (var i = 0; i < 81; i++) {
-  var tile = document.createElement("div");
-  tile.style.width = "11.1%";
-  tile.style.float = "left";
-  tile.style.paddingBottom = "11.1%";
-  if (i % 2 === 0) {
-    tile.style.backgroundColor = "pink";
-    body.appendChild(tile);
-  }
-  else {
-    tile.style.backgroundColor = "purple";
-    body.appendChild(tile);
-  }
+function randomColor () {
+  var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
 
+      }
+return color;
 }
+
+
+        function tiles (){
+          for (var i = 0; i < 81; i++){
+          // randomColor();
+        var tile = document.createElement("div");
+        tile.style.width = "11.1%";
+        tile.style.float = "left";
+        tile.style.paddingBottom = "11.1%";
+        tile.style.backgroundColor=(randomColor());
+        body.appendChild(tile);
+
+      }
+      return tile;
+      }
+
+      tiles();
+
+
+
+
+//   // if (i % 2 === 0) {
+//   //   tile.style.backgroundColor = "pink";
+//   //   body.appendChild(tile);
+//   // }
+//   // else {
+//   //   tile.style.backgroundColor = "purple";
+//   //   body.appendChild(tile);
+//   // }
+//
+// }
+
+
+
+
+
 
 
 
